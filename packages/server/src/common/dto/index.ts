88c12461ede3f45893a718247e4d11febad2c77b
mufeng <mufeng.me@gmail.com>
2024-03-28 19:56:24 +0800

    Add a whitelist host for the image resizing API

--- packages/server/src/common/dto/index.ts
+++ packages/server/src/common/dto/index.ts
@@ -1,9 +1,17 @@
 import { Transform } from 'class-transformer'
 import { IsInt, IsNumber, IsOptional, IsString, IsUrl } from 'class-validator'
+import { APP_HOMEPAGE_URL } from '@environments'
 
 export class ImageResizingDto {
   @IsUrl({
-    require_protocol: true
+    require_protocol: true,
+    host_whitelist: [
+      '127.0.0.1',
+      'localhost',
+      'images.unsplash.com',
+      'unsplash.com',
+      new URL(APP_HOMEPAGE_URL).hostname
+    ]
   })
   url: string
 
