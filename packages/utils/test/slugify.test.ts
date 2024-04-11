0d7285c8fdf979f91b2531e252d3aded7945ce88
mufeng <mufeng.me@gmail.com>
2024-04-11 12:19:01 +0800

    Add options to slugify

--- packages/utils/test/slugify.test.ts
+++ packages/utils/test/slugify.test.ts
@@ -2,4 +2,6 @@ import { slugify } from '../src'
 
 test('slugify', () => {
   expect(slugify('/user/sign/in')).toBe('usersignin')
+  expect(slugify('Hello World')).toBe('hello_world')
+  expect(slugify('Hello World', { replacement: '-' })).toBe('hello-world')
 })
