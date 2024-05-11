6a18e711693755384e0364d4300553f1cf3aa0b8
mufeng <mufeng.me@gmail.com>
2024-05-11 08:26:54 +0800

    Replace CDN resource `flag-icon` with npm package (close #84)

--- packages/webapp/src/pages/form/views/FormComponents/components/FlagIcon.tsx
+++ packages/webapp/src/pages/form/views/FormComponents/components/FlagIcon.tsx
@@ -1,6 +1,5 @@
 import clsx from 'clsx'
 import type { FC } from 'react'
-import { useEffect } from 'react'
 
 import type { IComponentProps } from '@/components/ui/typing'
 
@@ -9,18 +8,5 @@ export interface FlagIconProps extends IComponentProps {
 }
 
 export const FlagIcon: FC<FlagIconProps> = ({ className, countryCode = 'US' }) => {
-  useEffect(() => {
-    if (!document.getElementById('flag-icons')) {
-      const link = document.createElement('link')
-
-      link.id = 'flag-icons'
-      link.rel = 'stylesheet'
-      link.href =
-        'https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/6.4.4/css/flag-icons.min.css'
-
-      document.head.append(link)
-    }
-  }, [])
-
   return <span className={clsx(`fi fi-${countryCode?.toLowerCase()}`, className)} />
 }
