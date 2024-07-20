1c433c01b53b352444dd3821aff846ef78a1ba3c
mufeng <mufeng.me@gmail.com>
2024-07-20 10:29:03 +0800

    fix(embed): fix the issue of embedded form not rendering

--- packages/embed/src/index.ts
+++ packages/embed/src/index.ts
@@ -6,32 +6,43 @@ import { Standard } from './standard'
 import './style.scss'
 import { logger } from './utils'
 
-const cache = new Map<string, Modal<any> | Popup<any>>()
+const loaded = new Set<string>()
+const instances = new Map<string, Modal<any> | Popup<any>>()
 
-window.addEventListener('DOMContentLoaded', () => {
-  const configs = getConfigs()
+function main() {
+  try {
+    const configs = getConfigs()
 
-  configs.forEach(c => {
-    switch (c.type) {
-      case 'modal':
-        return cache.set(`${c.formId}-${c.type}`, new Modal(c))
+    configs.forEach(c => {
+      const key = `${c.formId}-${c.type}`
 
-      case 'popup':
-        return cache.set(`${c.formId}-${c.type}`, new Popup(c))
+      if (!loaded.has(key)) {
+        loaded.add(key)
 
-      case 'fullpage':
-        return new FullPage(c)
-
-      default:
-        return new Standard(c)
-    }
-  })
-
-  logger.info('Configs', JSON.stringify(configs))
-})
+        switch (c.type) {
+          case 'modal':
+            return instances.set(key, new Modal(c))
+    
+          case 'popup':
+            return instances.set(key, new Popup(c))
+    
+          case 'fullpage':
+            return new FullPage(c)
+    
+          default:
+            return new Standard(c)
+        }
+      }
+    })
+  
+    logger.info('Configs', JSON.stringify(configs))
+  } catch (err) {
+    logger.error(err)
+  }
+}
 
 function open(formId: string, type: 'modal' | 'popup') {
-  const instance = cache.get(`${formId}-${type}`)
+  const instance = instances.get(`${formId}-${type}`)
 
   if (instance) {
     instance.open()
@@ -39,7 +50,7 @@ function open(formId: string, type: 'modal' | 'popup') {
 }
 
 function close(formId: string, type: 'modal' | 'popup') {
-  const instance = cache.get(`${formId}-${type}`)
+  const instance = instances.get(`${formId}-${type}`)
 
   if (instance) {
     instance.close()
@@ -47,7 +58,7 @@ function close(formId: string, type: 'modal' | 'popup') {
 }
 
 function toggle(formId: string, type: 'modal' | 'popup') {
-  const instance = cache.get(`${formId}-${type}`)
+  const instance = instances.get(`${formId}-${type}`)
 
   if (instance) {
     instance.toggle()
@@ -60,3 +71,9 @@ export const toggleModal = (formId: string) => toggle(formId, 'modal')
 export const openPopup = (formId: string) => open(formId, 'popup')
 export const closePopup = (formId: string) => close(formId, 'popup')
 export const togglePopup = (formId: string) => toggle(formId, 'popup')
+
+// Import the library after the element
+main()
+
+// Import the library before the element
+window.addEventListener('DOMContentLoaded', main)
