1ea3c7236ba178df640663d77b425533208223e3
Frederik Kammer <frederik.kammer@osnaware.de>
2024-09-17 03:03:28 +0200

    Include hidden fields in csv export (#134)
    * Include hidden fields in csv export
    * Remove unused exportCsv method
    
    Close #116

--- packages/server/src/service/export-file.service.ts
+++ packages/server/src/service/export-file.service.ts
@@ -6,6 +6,7 @@ import {
   Answer,
   FieldKindEnum,
   FormField,
+  HiddenField,
   STATEMENT_FIELD_KINDS
 } from '@heyform-inc/shared-types-enums'
 import { helper, unixDate } from '@heyform-inc/utils'
@@ -18,18 +19,23 @@ const SUBMIT_DATE_KEY = 'Submit Date (UTC)'
 
 @Injectable()
 export class ExportFileService {
-  async csv(formFields: FormField[], submissions: SubmissionModel[]): Promise<string> {
+  async csv(
+    formFields: FormField[],
+    selectedHiddenFields: HiddenField[],
+    submissions: SubmissionModel[]
+  ): Promise<string> {
     const records: Record<string, any>[] = []
-    const selected = formFields
+    const selectedFormFields = formFields
       .filter(field => !STATEMENT_FIELD_KINDS.includes(field.kind))
-      .map(row => ({
-        ...row,
-        title: helper.isArray(row.title) ? htmlUtils.serialize(row.title) : row.title
+      .map(field => ({
+        ...field,
+        title: helper.isArray(field.title) ? htmlUtils.serialize(field.title) : field.title
       }))
 
     const fields: string[] = [
       FIELD_ID_KEY,
-      ...selected.map(row => row.title),
+      ...selectedFormFields.map(field => field.title),
+      ...selectedHiddenFields.map(hiddenField => hiddenField.name),
       START_DATE_KEY,
       SUBMIT_DATE_KEY
     ]
@@ -39,8 +45,8 @@ export class ExportFileService {
         [FIELD_ID_KEY]: submission.id
       }
 
-      for (const row of selected) {
-        let answer: any = submission.answers.find(answer => answer.id === row.id)
+      for (const field of selectedFormFields) {
+        let answer: any = submission.answers.find(answer => answer.id === field.id)
 
         if (helper.isEmpty(answer)) {
           answer = ''
@@ -48,7 +54,15 @@ export class ExportFileService {
           answer = this.parseAnswer(answer)
         }
 
-        record[row.title] = answer
+        record[field.title] = answer
+      }
+
+      for (const selectedHiddenField of selectedHiddenFields) {
+        const hiddenFieldValue = submission.hiddenFields.find(
+          hiddenField => hiddenField.id === selectedHiddenField.id
+        )?.value
+
+        record[selectedHiddenField.name] = hiddenFieldValue
       }
 
       record[START_DATE_KEY] = submission.startAt ? unixDate(submission.startAt!).toISOString() : ''
@@ -60,42 +74,6 @@ export class ExportFileService {
     return parseAsync(records, {
       fields
     })
-  }
-
-  exportCsv(formFields: FormField[], submissions: SubmissionModel[]): string {
-    const fields = formFields.filter(field => !STATEMENT_FIELD_KINDS.includes(field.kind))
-
-    const header: string[] = [
-      '#',
-      ...fields.map(field => `"${field.title}"`),
-      'Start Date (UTC)',
-      'Submit Date (UTC)'
-    ]
-    const records: string[] = [header.join(',')]
-
-    for (const submission of submissions) {
-      const record: string[] = [submission.id]
-
-      for (const field of fields) {
-        const answer = submission.answers.find(answer => answer.id === field.id)
-
-        if (helper.isEmpty(answer)) {
-          record.push('')
-        } else {
-          record.push(this.parseAnswer(answer))
-        }
-      }
-
-      const startAt = submission.startAt ? unixDate(submission.startAt!).toISOString() : ''
-      const endAt = submission.startAt ? unixDate(submission.endAt!).toISOString() : ''
-
-      record.push(startAt)
-      record.push(endAt)
-
-      records.push(record.join(','))
-    }
-
-    return records.join('\n')
   }
 
   private parseAnswer(answer: Answer): string {
